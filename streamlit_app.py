e221dde737152a98e8180ec5d20f8ff1754107f1
arora8067 <139110316+arora8067@users.noreply.github.com>
2023-07-10 13:39:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ my_fruit_list =my_fruit_list.set_index('Fruit')
 
 #lets put a pick list here so that user can pick the fruits they want
 
-fruits_selected = streamlit.multiselect('Pick Some Fruits: ',list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect('Pick Some Fruits: ',list(my_fruit_list.index)['Avocado','Strawberries'])
 fruits_to_show  = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
