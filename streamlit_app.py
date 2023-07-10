7b2dace5374c7d4d54fdc232d8e02ff1bd774f56
arora8067 <139110316+arora8067@users.noreply.github.com>
2023-07-10 18:39:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit
+import snowflake.connector
+import pandas as pd
+import requests
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
@@ -8,7 +15,7 @@ streamlit.text( '🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas as pd
+
 
 my_fruit_list = pd.read_csv( 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list =my_fruit_list.set_index('Fruit')
@@ -22,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice= streamlit.text_input('What fruit would you  like information about','Kiwi')
 streamlit.write('The user entered',fruit_choice)
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalize the response
@@ -31,10 +38,9 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # put it in a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+
+
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchall()
