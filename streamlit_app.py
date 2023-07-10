6a79eec8f06ab40507f1cfbe1ca65359a899cf6f
arora8067 <139110316+arora8067@users.noreply.github.com>
2023-07-10 18:46:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import snowflake.connector
 import pandas as pd
 import requests
+from urllib.error import URLError
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
